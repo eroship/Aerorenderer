8c8a0c14f1a73d29f728b6f7976366edf3fef051
eroship <y1035082558@163.com>
2023-05-12 22:27:37 +0800

    triangle

---
+++ geometry/geometry_painting.h
@@ -0,0 +1,17 @@
+#ifndef __GEOMETRY_PAINTING__
+#define __GEOMETRY_PAINTING__
+
+#include<iostream>
+#include "tgaimage.h"
+#include <vector>
+#include <cmath>
+#include "geometry.h"
+
+void line1(int x0, int y0, int x1, int y1, TGAImage &image, TGAColor color);
+void line2(int x0, int y0, int x1, int y1, TGAImage &image, TGAColor color);
+void line3(int x0, int y0, int x1, int y1, TGAImage &image, TGAColor color);
+void line(Vec2i p0, Vec2i p1, TGAImage &image, TGAColor color);
+
+void triangle(Vec2i t0, Vec2i t1, Vec2i t2, TGAImage &image, TGAColor color);
+
+#endif
