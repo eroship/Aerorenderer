83a9a1bd7b91f01965c05ca936369c470244fac2
eroship <y1035082558@163.com>
2023-05-12 21:23:03 +0800

    2023-5-12

--- test/test.cpp
+++ test/test.cpp
@@ -5,20 +5,89 @@
 const TGAColor white = TGAColor(255,255,255,255);
 const TGAColor red   = TGAColor(255,0,0,255);
 
-void line(int x0, int y0, int x1, int y1, TGAImage &image, TGAColor color) {
-    for (float t=0.; t<1.; t+=.1) {
+//第一种画线方法：指定两个顶点和步长(顶点的坐标是像素坐标)
+void line1(int x0, int y0, int x1, int y1, TGAImage &image, TGAColor color) {
+    for (float t=0.; t<1.; t+=.01) {
         int x = x0*(1.-t) + x1*t;
         int y = y0*(1.-t) + y1*t;
         image.set(x, y, color);
     }
 }
 
+//第二种画线方法：自动步长以及自动判断直线是否很陡
+//因为是像素坐标，所以让x每次前进一格就行，但是这样对于很陡的线就离散了
+//所以首先检测一下斜率是否大于1，如果是就xy倒一下
+void line2(int x0, int y0, int x1, int y1, TGAImage &image, TGAColor color){
+    bool steep = false;
+    int distx = std::abs(x0-x1);
+    int disty = std::abs(y0-y1);
+    if(distx<disty){
+        std::swap(x0,y0);
+        std::swap(x1,y1);
+        steep = true;
+    }
+    if(x0>x1){
+        std::swap(x0,x1);
+        std::swap(y0,y1);
+    }
+
+    for(int x=x0; x<=x1; x++){
+        float t = (x-x0)/(float(x1-x0));
+        int y = y0*(1.0-t)+y1*t;
+        if(steep)
+            image.set(y,x,color);
+        else 
+            image.set(x,y,color);
+    }
+}
+
+//第三种,不用每步计算除法
+//理论上，x每前进一格，y应该变换dy/dx格，但是由于像素坐标是离散的，所以会导致误差
+//如果某一格的误差>0.5，说明这一格的y该换行了
+//如果严格计算误差e，仍然需要除法：e每次增加dy/dx，大于0.5时换行：
+//e+dy/dx>0.5, e=e-1
+//做一次归一化避免计算除法：
+//2edx+2dy>dx, E=E-2dx(E=2edx)
+void line3(int x0, int y0, int x1, int y1, TGAImage &image, TGAColor color) {
+    bool steep = false;
+    int distx = std::abs(x0-x1);
+    int disty = std::abs(y0-y1);
+    if(distx<disty){
+        std::swap(x0,y0);
+        std::swap(x1,y1);
+        std::swap(distx,disty);
+        steep = true;
+    }
+    if(x0>x1){
+        std::swap(x0,x1);
+        std::swap(y0,y1);
+    }
+
+    int dy2 = disty*2;
+    int error2 = 0;
+    int y = y0;
+    for(int x=x0; x<=x1; x++){
+        if(steep)
+            image.set(y,x,color);
+        else 
+            image.set(x,y,color);
+        
+        error2 += dy2; //下一个点的二倍误差
+        if(error2 > distx){
+            y += (y1>y0?1:-1);
+            error2 -= distx*2;
+            //std::cout<<error2<<std::endl;
+        }
+    }
+}
+
 int main()
 {
     TGAImage image(100,100, TGAImage::RGB);
-    line(13,20,80,40,image,white);
+    //line3(13, 20, 80, 40, image, white); 
+    line3(20, 13, 40, 80, image, red); 
+    line3(80, 40, 13, 20, image, red);
     image.write_tga_file("1.tga");
-    image.flip_vertically();
-    image.write_tga_file("2.tga");
+    
     return 0;
 }
